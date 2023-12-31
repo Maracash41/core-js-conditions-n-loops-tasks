29f9ec5920c6f241f80801448b9dbea2e502e69b
Maracash41 <9531945@gmail.com>
2023-12-31 14:57:30 +0300

    feat: Task 02 - Done!

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,14 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let result = null;
+  if (a >= b && a >= c) {
+    result = a;
+  } else if (b >= a && b >= c) {
+    result = b;
+  }
+  return result;
 }
 
 /**
